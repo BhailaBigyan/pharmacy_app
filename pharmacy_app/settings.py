7c813aa2fb7b06d88da154807fd6dd0c12964e5e
Bigyan Bhaila <bigyanbhaila98@gmail.com>
2025-10-29 12:27:25 +0545

    ..

--- pharmacy_app/settings.py
+++ pharmacy_app/settings.py
@@ -13,7 +13,6 @@ SECRET_KEY = 'django-insecure-linfk6dl#^npscg$gt^70^xi9nx8e4duj=hp+@q2!38ouw84)%
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
 
@@ -114,7 +113,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
